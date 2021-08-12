2b2bf924f464291aa8c477aececde744d87a0e2a
joel <mizukaze_010@hotmail.com>
2021-08-12 01:19:22 -0600

    orfanize, add server.php,btn_effect.js

---
+++ src/js/view.js
@@ -0,0 +1,13 @@
+const interviews = [
+    { interview: "1", contenten: "" },
+    { interview: "2", contenten: "" },
+    { interview: "3", contenten: "" },
+    { interview: "5", contenten: "" },
+    { interview: "6", contenten: "" },
+    { interview: "7", contenten: "" },
+    { interview: "8", contenten: "" },
+    { interview: "9", contenten: "" },
+    { interview: "10", contenten: "" },
+];
+
+let choose = Math.floor(Math.random() * interviews.length);
