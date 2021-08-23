761f6080883c6f778793ecebacd3c518b84a8b91
joel <mizukaze_010@hotmail.com>
2021-08-23 09:55:44 -0600

    *

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let tiempoCaminata, tiempoespera, tiempoviaje, tarifa, transbordo, asiento;
 
 
 window.addEventListener("load", () => {
-    const random = Math.floor(Math.random() * 2/* interviews.length */);
+    const random = Math.floor(Math.random() * interviews.length);
     interview = interviews[random];
     console.log(interview);
     view.innerHTML = interview.content;
@@ -21,7 +21,6 @@ window.addEventListener("focusin", (e) => {
     console.log(target);
     switch (type) {
         case "button":
-            
             target.value = target.getAttribute("data-value");
             tracker(target)
             return;
@@ -31,7 +30,7 @@ window.addEventListener("focusin", (e) => {
 window.addEventListener("focusout", (e) => {
     let target = e.target;
     let { type } = target;
-    console.log(target);
+    console.log(interview.screens[screenCount].screen);
     switch (type) {
         case "button":
             target.value = "ver";
@@ -42,7 +41,7 @@ window.addEventListener("focusout", (e) => {
 
 function tracker({ name, value }) {
     let data = getLocalStorage();
-    data.push({ name, value });
+    data.push({ name, value, screen: interview.screens[screenCount].screen });
     saveData(data)
 }
 
@@ -53,7 +52,7 @@ function saveData(data) {
     return localStorage.setItem("tracker", JSON.stringify(data))
 }
 
-let page = document.querySelector(".page");
+let page = document.querySelector(".page.data");
 
 page.addEventListener("click", (e) => {
 
@@ -72,8 +71,11 @@ let divRandom = document.querySelector(".page.interview-random");
 divRandom.addEventListener("click", (e) => {
 
     if (e.target.className == "btn-next-screen") {
-        let { buttons } = interview.screens[++screenCount];
-        loadDataInterview(buttons)
+        if (++screenCount <= 7) {
+            console.log(screenCount);
+            let { buttons } = interview.screens[screenCount];
+            loadDataInterview(buttons)
+        }
     }
 
 })
@@ -82,24 +84,82 @@ function loadDataInterview(buttons) {
 
     buttons.forEach(button => {
         let value;
-        switch (button.row) {
-            case 1:
-                value = tiempoCaminata * button.value;
-                break;
-            case 2:
-                value = tiempoespera * button.value;
-                break;
-            case 3:
-                value = tiempoviaje * button.value;
-                break;
-            case 4:
-            case 5:
-                value = button.value;
-                break;
-            case 6:
-                value = asiento;
-                break;
+
+        if (interview.interview == "click-tracker-33" || interview.interview == "click-tracker-23") {
+            value = interview33_23(button.row, button.value)
+        }
+        else if (interview.interview == "click-tracker-32" || interview.interview == "click-tracker-22") {
+            value = interview32_22(button.row, button.value)
+        }
+        else {
+            value = generalInterview(button.row, button.value, asiento)
         }
         document.querySelector(`input[name=${button.button}]`).setAttribute("data-value", value);
     })
 }
+
+function generalInterview(row, value, asiento) {
+    switch (row) {
+        case 1:
+            return tiempoCaminata * value;
+        case 2:
+            return tiempoespera * value;
+        case 3:
+            return tiempoviaje * value;
+        case 4:
+        case 5:
+            return value;
+        case 6:
+            return asiento;
+
+    }
+}
+function interview33_23(row, value) {
+    switch (row) {
+        case 1:
+            return value = (tiempoCaminata + tiempoespera) * value;
+        case 2:
+            return tiempoviaje * value;
+        case 3:
+            return value;
+    }
+}
+
+function interview32_22(row, value) {
+    switch (row) {
+        case 1:
+            return (tiempoCaminata + tiempoespera + tiempoviaje) * value;
+        case 2:
+            return value;
+    }
+}
+
+function save(evt) {
+    evt.preventDefault();
+    let form = Object.fromEntries(new FormData(document.forms[0]))
+    let obj = new Object();
+    obj.name = interview.interview;
+    obj.email = form["pregunta-11"]
+    obj.question = [];
+    for (const key of Object.entries(form)) {
+        obj.question.push({ question: key[0], value: key[1] })
+    }
+    obj.tracks = JSON.parse(localStorage.getItem("tracker"))
+
+
+    fetch("http://localhost/pruebas/server.php", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(obj)
+    })
+        .then(r => {
+            console.log(r);
+            return r.json()
+        })
+        .then(d => console.log("resultado", d))
+
+}
+
+document.forms[0].addEventListener("submit", save);
